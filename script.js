481866246f3a4419a19816455e42eab7965f3bec
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-12-28 16:04:37 +0100

    adding confetti effect

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ $(document).ready(function () {
             $('#nameToDisplay').html(name);
             $('#prompt').addClass("hidden");
             $('#result').removeClass("hidden");
+            function createConfetti() {
+                var confetti = $('<div class="confetti"></div>');
+                var startX = Math.random() * window.innerWidth;
+                var rotate = Math.random() * 360;
+            
+                confetti.css({
+                  left: startX + 'px',
+                  transform: 'rotate(' + rotate + 'deg)',
+                });
+            
+                $('#confetti-container').append(confetti);
+            
+                setTimeout(function() {
+                  confetti.remove();
+                }, 2000); // Adjust the duration as needed
+              }
+            
+              function launchConfetti() {
+                setInterval(function() {
+                  createConfetti();
+                }, 100);
+              }
+            
+              launchConfetti();
         }
         else {
             alert("Please enter your name");
